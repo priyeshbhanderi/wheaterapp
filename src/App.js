9497fd3d6c20e9ca5591d61b1ff3d86aa4d04b10
pilobhanderi <166916729+pilobhanderi@users.noreply.github.com>
2024-07-16 16:02:00 +0530

    done

--- src/App.js
+++ src/App.js
@@ -34,73 +34,60 @@ function App() {
 
   return (
     <>
-      <div className='container-fluid p-5 bg-info px-5 d-flex justify-content-center align-items-center flex-column'>
-        <div className="d-flex w-100">
-          <input className="form-control me-2" type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} aria-label="Search" />
-          <button className="btn btn-outline-dark" onClick={searchPressed} type='submit'>Search</button>
-        </div>
-        {typeof weather.main !== "undefined" ? (
-          <>
-
-            <div className='row w-100 bg-white rounded-3 py-3'>
-              <div className='col-md-6 d-flex justify-content-center align-items-center flex-column'>
-                <span className='fs-1 mt-3 text-capitalize fw-bold'>{weather.name}</span>
-                <h1>{weather.main.temp}°C</h1>
-              </div>
-              <div className='col-md-6 d-flex justify-content-center align-items-center flex-column'>
-                <div className='card border-0'>
-                  <span className='fs-5 fw-bold'>Feels Like &nbsp;{weather.main.feels_like}°C</span>
-                  <div className='d-flex flex-row align-items-center mt-3'>
-                    <i className="fa-solid fa-up-long fs-4 me-2"></i>
-                    <span className='text-center fs-4 fw-bold '>{weather.main.temp_max}°C</span>
-                    <i className="fa-solid fa-down-long fs-4 mx-2 ms-4"></i>
-                    <span className='text-center fs-4 fw-bold '>{weather.main.temp_min}°C</span>
-                  </div>
-                  <div className='d-flex flex-row align-items-center mt-3'>
-                    <i className="bi bi-droplet-fill text-dark fs-4"></i>
-                    <span className='text-center fs-4 fw-bold mx-3'>Humidity</span>
-                    <span className='text-center fs-4 fw-bold '>{weather.main.humidity}%</span>
-                  </div>
-                  <div className='d-flex flex-row align-items-center mt-3'>
-                    <i className="bi bi-wind text-dark fs-4"></i>
-                    <span className='text-center fs-4 fw-bold mx-3'>Wind</span>
-                    <span className='text-center fs-4 fw-bold '>{weather.wind.speed} KPH</span>
+      <div className='weatherr'>
+        <div className='container-fluid d-flex flex-column justify-content-center align-items-center h-100'>
+          <div className="d-flex w-70">
+            <input className="form-control me-2" type="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} aria-label="Search" />
+            <button className="btn btn-outline-dark fw-bold fs-4" onClick={searchPressed} type='submit'>Search</button>
+          </div>
+          {typeof weather.main !== "undefined" ? (
+            <div className='row p-3 w-70'>
+              <div className='col-md-4 bggg'>
+                <span className='fs-2 text-white'><i className="bi bi-geo-alt-fill"></i> &nbsp;{weather.name}</span>
+                <h1 className='text-white'>{Math.round(weather.main.temp)} °C</h1>
+                <div className='card'>
+                  <div className='card border-0 d-flex align-items-start ps-5 justify-content-center text-white'>
+                    <span className='fs-5 '>Feels Like &nbsp; {weather.main.feels_like} °C</span>
+                    <div className='d-flex flex-row align-items-center mt-3'>
+                      <i className="fa-solid fa-up-long fs-4 me-2"></i>
+                      <span className='text-center fs-4  '>{Math.round(weather.main.temp_max)} °C</span>
+                      <i className="fa-solid fa-down-long fs-4 mx-2 ms-4"></i>
+                      <span className='text-center fs-4  '>{Math.round(weather.main.temp_min)} °C</span>
+                    </div>
+                    <div className='d-flex flex-row align-items-center mt-3'>
+                      <i className="bi bi-droplet-fill fs-4"></i>
+                      <span className='text-center fs-4  mx-3'>Humidity</span>
+                      <span className='text-center fs-4  '>{weather.main.humidity} %</span>
+                    </div>
+                    <div className='d-flex flex-row align-items-center mt-3'>
+                      <i className="bi bi-wind fs-4"></i>
+                      <span className='text-center fs-4  mx-3'>Wind</span>
+                      <span className='text-center fs-4  '>{weather.wind.speed} KPH</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className='row w-100 bg-white rounded-3 py-3 mt-4'>
-              <div className='col-12'>
-                <h2 className='mb-3 text-center'>7-Day Forecast</h2>
-                <div className='d-flex justify-content-around row row-cols-2 g-4'>
+              <div className='col-md-8 text-dark'>
+                <h2 className='my-3 text-end fs-1 fw-bold bggg p-2'>7-Day Forecast</h2>
+                <div className='d-flex justify-content-around row row-cols-3 g-4 w-100 mt-5'>
                   {forecast.map((day, index) => (
                     <div className='col'>
-                      <div key={index} className='d-flex flex-column align-items-center'>
-                        <span className='fw-bold'>{new Date(day.dt * 1000).toLocaleDateString("en-US", { weekday: 'long' })}</span>
-                        <span>{day.temp.day}°C</span>
-                        <span className='text-capitalize'>{day.weather[0].description}</span>
+                      <div key={index} className='d-flex flex-column align-items-center bggg'>
+                        <span className='fw-bold fs-4'>{new Date(day.dt * 1000).toLocaleDateString("en-US", { weekday: 'long' })}</span>
+                        <span className='fs-4'>{Math.round(day.temp.day)}°C</span>
+                        <span className='text-capitalize fs-5'>{day.weather[0].description}</span>
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
-            </div>
-          </>
-        ) : (
-          <p className='text-white fs-3'>Enter a city name to get the weather information</p>
-        )}
-      </div>
 
-
-      <div className='weatherr'>
-        <div className='container-fluid d-flex justify-content-center align-items-center'>
-          <div className='row bg-white'>
-            <div className='col-md-4'>
-                  <span>surat</span>
             </div>
-            <div className='col-md-8'></div>
-          </div>
+          ) : (
+            <p className='text-white fs-3'>Enter a city name to get the weather information</p>
+          )}
         </div>
+
       </div>
     </>
   );
